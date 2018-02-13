859f6fc2356fc03776258eed55fa015cece97317
Danny Robinson <dannyrobinson@gmx.com>
2018-02-13 12:07:42 +0000

    Added the main icon
    Battering away at this thing trying to figure out Python. At least I got
    the icon working.

--- lbry.py
+++ lbry.py
@@ -2,9 +2,12 @@
 
 from resources.lib import kodilogging
 from resources.lib import plugin
+from resources.lib import requests
 
 import logging
 import xbmcaddon
+
+import json
 
 # Keep this file to a minimum, as Kodi
 # doesn't keep a compiled copy of this
@@ -12,5 +15,3 @@ ADDON = xbmcaddon.Addon()
 kodilogging.config()
 
 plugin.run()
-
-
