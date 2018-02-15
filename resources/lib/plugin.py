825bd34afafbca69440f58813f798375514bd5f4
Danny Robinson <dannyrobinson@gmx.com>
2018-02-15 00:48:19 +0000

    Started on menu layout, error message now shows on failed attempt to lbrynet_daemon

--- resources/lib/plugin.py
+++ resources/lib/plugin.py
@@ -5,7 +5,7 @@ import logging
 import xbmcaddon
 from resources.lib import kodiutils
 from resources.lib import kodilogging
-from xbmcgui import ListItem
+from xbmcgui import ListItem, Dialog
 from xbmcplugin import addDirectoryItem, endOfDirectory
 import requests
 
@@ -13,21 +13,39 @@ ADDON = xbmcaddon.Addon()
 logger = logging.getLogger(ADDON.getAddonInfo('id'))
 kodilogging.config()
 plugin = routing.Plugin()
-
+dialog = Dialog()
 
 @plugin.route('/')
 def index():
     addDirectoryItem(plugin.handle, plugin.url_for(
-        show_category, "one"), ListItem("Connect to spee.ch"), True)
+        speech_menu), ListItem("Connect via web portal"), True)
     addDirectoryItem(plugin.handle, plugin.url_for(
-        show_category, "two"), ListItem("Connect to lbry daemon"), True)
+        connect_to_daemon), ListItem("Connect via local daemon"), True)
     endOfDirectory(plugin.handle)
 
+@plugin.route('/lbry/connect')
+def connect_to_daemon():
+    try:
+        result = requests.post('http://localhost:5279', json={"method":"settings_get"})
+        result.raise_for_status()
+    except:
+        dialog.ok('Error', ('Could not connect to the lbrynet daemon. Make sure'
+            ' that the daemon is running and that the address is configured.'))
 
-@plugin.route('/category/<category_id>')
-def show_category(category_id):
-    addDirectoryItem(
-        plugin.handle, "", ListItem("Hello category %s!" % category_id))
+@plugin.route('/speech')
+def speech_menu():
+    addDirectoryItem(plugin.handle, plugin.url_for(speech_search), ListItem("Search"))
+    endOfDirectory(plugin.handle)
+
+@plugin.route('/speech/search')
+def speech_search():
+    query = dialog.input('search on spee.ch')
+
+@plugin.route('/lbry/localfiles')
+def show_localfiles():
+    result = requests.post('http://localhost:5279', json={"method"})
+    # get the local lbry directory
+    # create an entry (thumbnail?) for each file
     endOfDirectory(plugin.handle)
 
 def run():
